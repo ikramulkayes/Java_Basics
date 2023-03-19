871542d48e71c75ba42d7921ab385bc7b5a1d26e
MD Ikramul Kayes <67923321+ikramulkayes@users.noreply.github.com>
2023-03-20 02:36:32 +0600

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.awt.desktop.SystemEventListener;
+import java.util.Date;
+public class Main {
+    public static void main(String[] args) {
+        int myAge = 23;
+        int moAge = 30;
+        int baby = myAge + moAge;
+        System.out.println(baby);
+        long boroBaby = 10_10_9;
+        float captain = 10.99f;
+        char word = 'b';
+        boolean flag = true;
+        System.out.println(boroBaby);
+        Date now = new Date();
+        System.out.println(now);
+        
+    }
+}

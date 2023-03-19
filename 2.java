075f26744464fc262923a7f0c322e253bbe92539
MD Ikramul Kayes <67923321+ikramulkayes@users.noreply.github.com>
2023-03-20 02:40:40 +0600

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.awt.desktop.SystemEventListener;
+import java.util.Date;
+public class Main {
+    public static void main(String[] args) {
+        String baby = "Wassap yo";
+        System.out.println(baby);
+        baby = baby + "how you doin";
+        System.out.println(baby);
+        System.out.println(baby);
+        baby = "         yo     ";
+        baby = baby.trim();
+        System.out.println(baby);
+
+    }
+}

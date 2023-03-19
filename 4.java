07fd48333348c5d951fbb8ce242e3250171cb5e2
MD Ikramul Kayes <67923321+ikramulkayes@users.noreply.github.com>
2023-03-20 03:28:58 +0600

    Create 4.java

---
+++ 4.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.desktop.SystemEventListener;
+import java.util.Arrays;
+import java.util.Date;
+public class Main {
+    public static void main(String[] args) {
+        int[] lst = {1,4,2,5};
+        Arrays.sort(lst);
+        System.out.println(Arrays.toString(lst));
+
+    }
+}

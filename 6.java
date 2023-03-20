d2967156bf945f8cf2b90a932ab93bdf1e71f41d
MD Ikramul Kayes <67923321+ikramulkayes@users.noreply.github.com>
2023-03-20 13:23:42 +0600

    Create 6.java

---
+++ 6.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.awt.desktop.SystemEventListener;
+import java.util.Arrays;
+import java.util.Date;
+public class Main {
+    public static void main(String[] args) {
+        short a = 1;
+        int b = a +1;
+        System.out.println(b);
+        String x = "1";
+        int y = Integer.parseInt(x);  //converting
+        System.out.println(y);
+        x = "1.1";
+        double gg = Double.parseDouble(x);  //converting
+        System.out.println(gg);
+
+
+    }
+}

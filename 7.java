2014ab47f869669f2da01c6b2792d44682108f08
MD Ikramul Kayes <67923321+ikramulkayes@users.noreply.github.com>
2023-03-20 13:38:47 +0600

    Create 7.java

---
+++ 7.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.desktop.SystemEventListener;
+import java.util.Arrays;
+import java.util.Date;
+public class Main {
+    public static void main(String[] args) {
+        int x = (int )(Math.round(Math.random()*100)); //explicit casting
+        System.out.println(x);
+
+
+    }
+}

70417b640256a391f406f1522745d0e0b62c15e2
MD Ikramul Kayes <67923321+ikramulkayes@users.noreply.github.com>
2023-03-22 00:52:26 +0600

    Create 11.java

---
+++ 11.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.awt.desktop.SystemEventListener;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String a = "baby";
+        if (a == "cute"){
+            System.out.println("you are cute");
+        }
+        else if (a== "baby"){
+            System.out.println("Baby");
+
+        }else{
+            System.out.println("PASS");
+        }
+
+    }
+}

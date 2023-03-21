92a5ac7692beef1891e23d290f29173f621cecb9
MD Ikramul Kayes <67923321+ikramulkayes@users.noreply.github.com>
2023-03-22 01:42:12 +0600

    Create 14.py

---
+++ 14.py
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.awt.desktop.SystemEventListener;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] fruits = {"Apple","Mango","Orange"};
+        for (String fruit: fruits){   //for each loop
+            System.out.println(fruit);
+        }
+
+    }
+}

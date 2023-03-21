1abf6dea716496ab308bb81b610fe5704d22e81f
MD Ikramul Kayes <67923321+ikramulkayes@users.noreply.github.com>
2023-03-22 00:08:45 +0600

    Create 10.java

---
+++ 10.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.desktop.SystemEventListener;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean a = false;
+        boolean b = true;
+        boolean c = false;
+        boolean res = (a ||b) && !c; // or and not
+        System.out.println(res);
+
+
+    }
+}
